e73fcbf9984151d8e8e805d451a8d5f75ea0e3c8
GregApple <63629336+GregApple@users.noreply.github.com>
2020-09-29 12:20:07 -0700

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -1,11 +1,22 @@
+
 # Create your Phrase class logic here.
 class Phrase:
-  
-    def display(active_phrase, guesses):
+
+    # The class should include an initializer or def __init__ that receives a phrase parameter and holds this phrase in an instance attribute on the Phrase object.
+    def __init__(self, phrase):
+        # phrase: this is the actual phrase the Phrase object is representing. This attribute should be set to the phrase parameter but converted to all lower case.
+        self.my_phrase = phrase
+
+        
+    def __str__(self):
+        return self.my_phrase
+
+        
+    def display(self, guesses):
       # this prints out the phrase to the console with guessed letters visibile and unguessed letters as underscores.
-      new_phrase = "" 
-      
-      for letter in active_phrase:
+      new_phrase = ""
+      my_phrase = str(self.my_phrase)
+      for letter in my_phrase:
         if letter.isalpha():
           if letter in guesses:
             new_phrase = new_phrase + letter
@@ -17,15 +28,17 @@ class Phrase:
       return new_phrase
         
         
-    def check_letter(active_phrase, guess):
+    def check_letter(self, guess):
       # checks to see if the letter selected by the user matches a letter in the phrase.
-      response = active_phrase.count(guess) > 0
+      my_phrase = str(self.my_phrase)
+      response = my_phrase.count(guess) > 0
         
       return response  
     
   
-    def check_complete(active_phrase, new_phrase):
+    def check_complete(self, new_phrase):
         # checks to see if the whole phrase has been guessed.
-        complete = active_phrase.upper() == new_phrase.upper()
+        my_phrase = str(self.my_phrase)
+        complete = my_phrase.upper() == new_phrase.upper()
           
         return complete
